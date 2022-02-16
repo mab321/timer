5f89814f4f8dc62119e78f921fd6adb9d5da4545
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-16 04:13:46 +0000

    Added complete timer1.js pinging based on passed time windows in seconds

---
+++ timer1.js
@@ -0,0 +1,16 @@
+// filter process.arv from nonNumbers and numbers less than 0;
+const args = process.argv.filter(elm => !isNaN(elm) && elm >= 0);
+
+
+const setAlarm = function(alarms) {
+  // Create new array populated with numbers;
+  let timers = alarms.map(a => Number(a));
+  
+  for ( const elm of timers) {
+    setTimeout(() => { 
+      process.stdout.write(`${elm} seconds \x07\n`);
+    },elm * 1000);
+  }
+}
+
+setAlarm(args);
